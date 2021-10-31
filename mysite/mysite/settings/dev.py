dd95981cc344633556736ecf878afa6aa11aeec6
Andres Palma <palma.castaneda030@gmail.com>
2021-10-31 15:46:01 -0500

    cambios con el framework wagtail

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q*#j-n)a^%qd-*w$%2c=1=y7*to#y$hi_smbhn#_(4qu+=+))6'
+SECRET_KEY = 'django-insecure-1#v^txc$jte*yl@8m_7r+b3^(_h3__#v6o9v-p6j=y9_o#&y&1'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
